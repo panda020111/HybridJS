e7b1efefd5742d1e740327617fafddca04a3dd7e
panda020111 <panda020111@gmail.com>
2018-05-24 17:08:42 +0800

    feat: add file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="handleClick">点击触发事件</button>
+    <button @click="handleClick">getDeviceInfo</button>
+    <button @click="handleClickInfo">alertInfo</button>
   </div>
 </template>
 
@@ -22,6 +23,14 @@ export default {
       }, function () {
         console.log('error message')
       }, 'hybrid.device', 'getDeviceInfo', ["hello world"])
+    },
+    handleClickInfo () {
+      console.log('in click info')
+      exec(function (data) {
+        console.log(JSON.stringify(data))
+      }, function (err) {
+        console.log(err)
+      }, 'hybrid.notification', 'alert', ["title", "Hello world alert"])
     }
   }
 }
